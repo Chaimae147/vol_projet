182a4411c322783a149df2ef3c0c54b2a8f82f4f
chaimae147 <chaymae902@gmail.com>
2023-03-21 17:27:57 +0000

    modification

--- src/composents/navbar/Navbar.js
+++ src/composents/navbar/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar() {
 
         <li className="nav-item"><a href="/" style={{ marginRight:'50px' }} className="nav-link">Acceuil</a></li>
         <li className="nav-item"><a href="/about" style={{ marginRight:'50px' }} className="nav-link">About</a></li>
-        <li className="nav-item"><a href="/Reservatiom" style={{ marginRight:'200px' }} className="nav-link" >Reservation</a></li>
+        <li className="nav-item"><a href="/Reservatiom" style={{ marginRight:'30vh' }} className="nav-link" >Reservation</a></li>
         <li className='nav-item'><a href="/login" className='nav-link' style={{  marginRight:'15px' }}>  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>

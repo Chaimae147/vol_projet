be38419910b3a608537d9233d07a9bc54e6ae7d7
chaimae147 <chaymae902@gmail.com>
2023-04-25 01:27:36 +0100

    reservation

--- src/composents/Reservation/Reservation.js
+++ src/composents/Reservation/Reservation.js
@@ -6,15 +6,43 @@ import ticket from './reservation-billet-avion-ligne-appareil-concept-vol-voyage
 import { useState } from 'react'
 import axios from 'axios';
 export default function Reservation() {
-  const [step, setStep] = useState(1);
-  const [origine, setOrigine] = useState('');
-  const [destination, setDestination] = useState('');
-  const [dateDepart, setDepart] = useState('');
-  const [dateRetour, setRetour] = useState('');
-  const [classe, setClasse] = useState('');
-  const [codereduction, setReduction] = useState('');
-  const [passagers, setPassagers] = useState('');
-  const [modePaiment, setModepay] = useState('');
+  const [formData, setFormData] = useState({
+    origine: '',
+    destination: '',
+    dateDepart: '',
+    dateRetour: '',
+     classe: '',
+    passagers:'',
+    codereduction: '', 
+    //modePaiment: '',
+
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://127.0.0.1:8000/api/ajouterreservation', formData)
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+        setMessage('Une erreur est survenue. Veuillez réessayer plus tard.');
+      });
+  };
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+   const [step, setStep] = useState(1);
+  /*const [origine, setOrigine] = useState();
+  const [destination, setDestination] = useState();
+  const [dateDepart, setDepart] = useState();
+  const [dateRetour, setRetour] = useState();
+  const [classe, setClasse] = useState();
+  const [codereduction, setReduction] = useState();
+  const [passagers, setPassagers] = useState();
+  const [modePaiment, setModepay] = useState();
+    */
   const[message,setMessage]=useState('');
   const handleNext = () => {
     setStep(step + 1);
@@ -24,14 +52,12 @@ export default function Reservation() {
     setStep(step - 1);
   };
 
-  const handleSubmit = () => {
-    // Handle form submission here
-  };
+  
   
 
   const progress = ((step - 1) / 4) * 100; // Assuming three steps
-  const ajouterreservation = () => {
-    axios.post('http://127.0.0.1:8000/api/reservations', { origine, destination, dateDepart , classe, passagers,codereduction,modePaiment })
+  /* const ajouterreservation = () => {
+    axios.post('http://127.0.0.1:8000/api/ajoutereservation', { origine, destination, dateDepart , classe, passagers,codereduction,modePaiment })
   .then((response) => {
     setMessage(response.data.message);
     setOrigine('');
@@ -59,7 +85,10 @@ export default function Reservation() {
   setPassagers('');
   setReduction('');
   setModepay('');
-} 
+}
+
+
+value={origine} onChange={(e) => setOrigine(e.target.value)}*/
   return (
 
     <div className='backgroundss'>
@@ -72,21 +101,21 @@ export default function Reservation() {
         <div style={{ width: `${progress}%`, height: '100%', backgroundColor: 'blue' }} />
       </div>
       {step === 1 && (
-        <form action="">
+        <form action="POST">
          <div className="form-content">
          <div className='reser-form'>
             <div className='input-boxes'>
               <div className='input-box'>
-                <input type="text" placeholder="Selectionnez l'origine" value={origine} onChange={(e) => setOrigine(e.target.value)}  required/>
+                <input type="text"name='origine' placeholder="Selectionnez l'origine"   value={formData.origine} onChange={handleInputChange}/>
               </div>
               <div className='input-box'>
-                <input type="text" placeholder='Selectionnez destination' value={destination} onChange={(e) => setDestination(e.target.value)}  required/>
+                <input type="text" name='destination' placeholder='Selectionnez destination' value={formData.destination} onChange={handleInputChange}/>
               </div>
               <div className='input-box'>
-              <input type="text" placeholder="Date Depart" value={dateDepart} onChange={(e) => setDepart(e.target.value)}  onFocus={(e) => e.currentTarget.type = "date"} onBlur={(e) => e.currentTarget.type = "text"} />
+              <input type="text" name='dateDepart' placeholder="Date Depart" value={formData.dateDepart} onChange={handleInputChange}  onFocus={(e) => e.currentTarget.type = "date"} onBlur={(e) => e.currentTarget.type = "text"} />
               </div>
               <div className=' input-box'>
-              <input type="text" placeholder="Date Retour" value={dateRetour} onChange={(e) => setRetour(e.target.value)}  onFocus={(e) => e.currentTarget.type = "date"} onBlur={(e) => e.currentTarget.type = "text"} />
+              <input type="text" name='dateRetour' placeholder="Date Retour" value={formData.dateRetour} onChange={handleInputChange}  onFocus={(e) => e.currentTarget.type = "date"} onBlur={(e) => e.currentTarget.type = "text"} />
               </div>
               <div className='button input-box'>
                 <input type="button" onClick={handleNext} style={{ marginLeft:'70%' }} value='SUIVANT >'/>
@@ -101,13 +130,13 @@ export default function Reservation() {
         
       )}
       {step === 2 && (
-        <form action="">
+        <form action="POST">
          <div className="form-content">
          <div className='reser-form'>
             <div className='input-boxes'>
               <div className='input-box'>
                 <label> 
-                  <select value={classe} onChange={(e) => setClasse(e.target.value)} style={{   width: '370px',color:'#6e6e6e' }}>
+                  <select name='classe' value={formData.classe} onChange={handleInputChange} style={{   width: '370px',color:'#6e6e6e' }}>
                   <option value="" disabled>-- Selectionnez Classe --</option>
                     <option value="economy">Economique</option>
                     <option value="business">Affaires</option>
@@ -118,7 +147,7 @@ export default function Reservation() {
               
                 <div className='input-box'>
                 <label>
-                  <select  value={passagers} onChange={(e) => setPassagers(e.target.value)} style={{   width: '370px' ,color:'#6e6e6e'}}>
+                  <select name='passagers' value={formData.classe} onChange={handleInputChange} style={{   width: '370px' ,color:'#6e6e6e'}}>
                   <option value="" disabled>-- Selectionnez Passagers --</option>
 
                     <option value="ADULTES">ADULTES</option>
@@ -129,7 +158,7 @@ export default function Reservation() {
                 
                 </div>
                 <div className='input-box'>
-                  <input type="text" placeholder="CODE DE REDUCTION" value={codereduction} onChange={(e) => setReduction(e.target.value)} required/>
+                  <input name='codereduction' type="text" placeholder="CODE DE REDUCTION" value={formData.codereduction} onChange={handleInputChange}/>
                 </div>
                 <br />
                 <br />
@@ -146,28 +175,27 @@ export default function Reservation() {
         </form>
       )}
       {step === 3 && (
-        <form action="">
+        <form action="POST">
         <div className="form-content">
         <div className='reser-form'>
           
            <div className='tittle2'>MODE DE PAIMENT : </div>
            <div className='input-boxes'>
-             <div className='input-box1'>
-               <input type="radio" name='paiment' value='carteBancarire' checked={modePaiment === 'carteBancarire'} onChange={(e) => setModepay(e.target.value)} />&nbsp;&nbsp;&nbsp; CARTE BANCAIRE
+            {/*  <div className='input-box1'>
+               <input type="radio" name='modePaiment' value='carteBancarire' checked={formData.modePaiment === 'carteBancarire'}  onChange={handleInputChange} />&nbsp;&nbsp;&nbsp; CARTE BANCAIRE
              </div>
              <div className='input-box1'>
-               <input type="radio"  name='paiment' value='carteInternationale' checked={modePaiment === 'carteInternationale'} onChange={(e) => setModepay(e.target.value)} />&nbsp;&nbsp;&nbsp;CARTE BANCAIRE INTERNATIONALE
-             </div>
-             
-             <div className='input-box1'>
-               <input type="radio"  name='paiment' value='paimentespeces' checked={modePaiment === 'paimentespeces'} onChange={(e) => setModepay(e.target.value)} />&nbsp;&nbsp;&nbsp;PAIMENT EN ESPECES
+               <input type="radio"  name='modePaiment' value='carteInternationale' checked={formData.modePaiment === 'carteBancarire'}  onChange={handleInputChange} />&nbsp;&nbsp;&nbsp;CARTE BANCAIRE INTERNATIONALE
              </div>
              
+             <div className='input-box1'>
+               <input type="radio"  name='modePaiment' value='paimentespeces' checked={formData.modePaiment === 'carteBancarire'}  onChange={handleInputChange}/>&nbsp;&nbsp;&nbsp;PAIMENT EN ESPECES
+             </div>
+              */}
              <div className='button input-box'>
              <input type="button" onClick={handlePrev}  className='' style={{ marginLeft:'0' }}  value='< PRECEDENT'/>
      
-               <input type="submit"  onClick={ajouterreservation} className='' value='RESERVER'/>
-               {message}
+               <input type="submit"  onClick={handleSubmit} className='' value='RESERVER'/>
              </div>
            </div>
          </div>
@@ -176,7 +204,10 @@ export default function Reservation() {
          </div>
         </div>
        </form>
+
       )}
+      <div>               {message}
+</div>
  {/*  <PremiereStep/>
   <DeuxiemeStep/>
   <TroisiemeStep/> */}
